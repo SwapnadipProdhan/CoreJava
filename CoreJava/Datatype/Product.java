776090de8ed1c78efac80f5730b0f1486e806ed2
SwapnadipProdhan <142204350+SwapnadipProdhan@users.noreply.github.com>
2023-11-24 12:06:52 +0530

    Add files via upload

---
+++ CoreJava/Datatype/Product.java
@@ -0,0 +1,10 @@
+public class Product {
+
+	public static void main(String[] args) {
+		double num1 = 6.3;
+        int num2 = 8;
+        double product = num1 * num2;
+        System.out.println("The product is: "+ product);
+	}
+
+}
---
+++ CoreJava/String/CompareStrings.java
@@ -0,0 +1,12 @@
+public class CompareStrings {
+	public static void main(String[] args) {
+        String string1 = "India";
+        String string2 = "iNDIA";
+        boolean isequal = string1.equalsIgnoreCase(string2);
+        if (isequal) {
+            System.out.println("The strings are equal.");
+        } else {
+            System.out.println("The strings are not equal.");
+        }
+    }
+}
